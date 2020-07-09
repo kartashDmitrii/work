3d81f20c3276170dac8f9dc3e1b5e4e3aabdc411
kartashDmitrii <kartaschdim@gmail.com>
2020-07-09 17:32:56 +0300

    'v3.0'

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 window.onload = () => {
-    // make dependent height
+    let checkFlag = window.getComputedStyle(document.querySelector('header .burger')).getPropertyValue('display');
     let header = document.querySelector('header');
     let footer = document.querySelector('footer');
     let headerHeight = Math.floor(parseInt(window.getComputedStyle(header).getPropertyValue('height')));
     let footerHeight = Math.floor(parseInt(window.getComputedStyle(footer).getPropertyValue('height')));
-    document.querySelector('main').style.marginTop = headerHeight + 'px';
-    document.querySelector('main').style.marginBottom = footerHeight + 'px';
+    if (checkFlag === 'block') {
+        // make dependent height
 
-    // document.querySelector('.download-free').style.bottom = Math.floor(parseInt(window.getComputedStyle(footer).getPropertyValue('height'))) + 20 + 'px';
-    document.querySelectorAll('.block').forEach( (elem)=> {
-        elem.style.minHeight = window.innerHeight - headerHeight - footerHeight + 'px'
-    });
-    document.querySelectorAll('.popup').forEach( (elem)=> {
-        elem.style.paddingTop = headerHeight + 'px';
-        elem.style.paddingBottom = footerHeight + 'px';
-    });
+        document.querySelector('main').style.marginTop = headerHeight + 'px';
+        document.querySelector('main').style.marginBottom = footerHeight + 'px';
+
+        document.querySelector('.download-free').style.bottom = Math.floor(parseInt(window.getComputedStyle(footer).getPropertyValue('height'))) + 20 + 'px';
+        document.querySelectorAll('.block').forEach((elem) => {
+            elem.style.minHeight = window.innerHeight - headerHeight - footerHeight + 'px'
+        });
+        document.querySelectorAll('.popup').forEach((elem) => {
+            elem.style.paddingTop = headerHeight + 'px';
+            elem.style.paddingBottom = footerHeight + 'px';
+        });
+    }
     // make dependent height
 
     // slider
@@ -46,12 +50,14 @@ window.onload = () => {
         })
         document.querySelector('.catalog-slider .dots ul').appendChild(li);
     });
+    document.querySelector('.catalog-slider .arrows .next').addEventListener('click', () => siema.next());
+    document.querySelector('.catalog-slider .arrows .prev').addEventListener('click', () => siema.prev());
 
     // slider
 
     // smooth-scroll
 
-    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+    const anchors = [].slice.call(document.querySelectorAll('*[href*="#"]'));
     anchors.forEach(function(item) {
         item.addEventListener('click', function(e) {
             e.preventDefault();
@@ -72,70 +78,73 @@ window.onload = () => {
     });
 
     // smooth-scroll
-
     // btn-events
+    if (checkFlag === 'block') {
+        document.querySelector('header .menu .burger button').addEventListener('click', function () {
+            if(document.querySelector('.popups .language').style.display === 'flex'){
+                document.querySelector('.popups .language').style.right = '-100%';
+                setTimeout( ()=>{
+                    document.querySelector('.popups .language').style.display = 'none'
+                }, 300)
+            }
+            let popup = document.querySelector('.popups .menu');
+            if(popup.style.display === 'flex'){
+                document.querySelector('html').style.overflowY = 'scroll';
+                popup.style.left = '-100%';
+                setTimeout( ()=>{
+                    popup.style.display = 'none'
+                }, 300)
+            } else {
+                document.querySelector('html').style.overflowY = 'hidden';
+                popup.style.display = 'flex';
+                setTimeout( ()=>{
+                    popup.style.left = '0';
+                }, 0)
 
-    document.querySelector('header .menu .burger button').addEventListener('click', function () {
-        if(document.querySelector('.popups .language').style.display === 'flex'){
-            document.querySelector('.popups .language').style.right = '-100%';
-            setTimeout( ()=>{
-                document.querySelector('.popups .language').style.display = 'none'
-            }, 300)
-        }
-        let popup = document.querySelector('.popups .menu');
-        if(popup.style.display === 'flex'){
-            document.querySelector('html').style.overflowY = 'scroll';
-            popup.style.left = '-100%';
-            setTimeout( ()=>{
-                popup.style.display = 'none'
-            }, 300)
-        } else {
-            document.querySelector('html').style.overflowY = 'hidden';
-            popup.style.display = 'flex';
-            setTimeout( ()=>{
-                popup.style.left = '0';
-            }, 0)
+            }
+        });
 
-        }
-    });
-    document.querySelector('header .menu .language').addEventListener('click', function () {
-        if(document.querySelector('.popups .menu').style.display === 'flex'){
-            document.querySelector('.popups .menu').style.left = '-100%';
-            setTimeout( ()=>{
-                document.querySelector('.popups .menu').style.display = 'none'
-            }, 300)
-        }
-        let popup = document.querySelector('.popups .language');
-        if(popup.style.display === 'flex'){
-            document.querySelector('html').style.overflowY = 'scroll';
-            popup.style.right = '-100%';
-            setTimeout( ()=>{
-                popup.style.display = 'none'
-            }, 300)
-        } else {
-            document.querySelector('html').style.overflowY = 'hidden';
-            popup.style.display = 'flex';
-            setTimeout( ()=>{
-                popup.style.right = '0';
-            }, 0)
+        document.querySelector('header .menu .language').addEventListener('click', function () {
+            if (document.querySelector('.popups .menu').style.display === 'flex') {
+                document.querySelector('.popups .menu').style.left = '-100%';
+                setTimeout(() => {
+                    document.querySelector('.popups .menu').style.display = 'none'
+                }, 300)
+            }
+            let popup = document.querySelector('.popups .language');
+            if (popup.style.display === 'flex') {
+                document.querySelector('html').style.overflowY = 'scroll';
+                popup.style.right = '-100%';
+                setTimeout(() => {
+                    popup.style.display = 'none'
+                }, 300)
+            } else {
+                document.querySelector('html').style.overflowY = 'hidden';
+                popup.style.display = 'flex';
+                setTimeout(() => {
+                    popup.style.right = '0';
+                }, 0)
 
-        }
-    });
+            }
+        });
+    }
 
     // btn-events
 
     // show/hide header
 
-    let prevScrollpos = window.pageYOffset;
-    window.addEventListener('scroll', function() {
-        let currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.querySelector('header').style.top = "0";
-        } else {
-            document.querySelector('header').style.top = `-${headerHeight * 3}px`;
-        }
-        prevScrollpos = currentScrollPos;
-    })
+    if (checkFlag === 'block') {
+        let prevScrollpos = window.pageYOffset;
+        window.addEventListener('scroll', function () {
+            let currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.querySelector('header').style.top = "0";
+            } else {
+                document.querySelector('header').style.top = `-${headerHeight * 3}px`;
+            }
+            prevScrollpos = currentScrollPos;
+        })
+    }
 
     // show/hide header
 };
